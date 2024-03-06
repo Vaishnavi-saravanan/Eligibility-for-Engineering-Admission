cae92a11681318248d8c46e7c3021b6d0bd8b7ce
Vaishnavi-Saravanan <vaishanavisaravanan2729@gmail.com>
2024-03-06 20:19:37 +0530

    updated aim, result and partial algorithm

--- Program.cs
+++ Program.cs
@@ -1,9 +1,38 @@
 ﻿using System;
-    class Eligibility
+
+namespace Admission
+{
+    class Program
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hiiiiiii");
+            int maths, physics, chemistry;
+            Console.WriteLine("Welcome to Engineering Admission Eligibility Checker");
+
+            Console.Write("Enter Math marks: ");
+            maths = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Physics marks: ");
+            physics = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Chemistry marks: ");
+            chemistry = Convert.ToInt32(Console.ReadLine());
+
+            int totalMarks = maths + physics + chemistry;
+
+            if (maths >= 75 && physics >= 65 && chemistry >= 60)
+            {
+                if (totalMarks >= 180)
+                {
+                    Console.WriteLine("Congratulations! You are eligible for admission.");
+
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you are not eligible for admission.");
+
+                }
+            }
         }
     }
-
+}
